91410aa8d4276246579298c65f9859676de094f8
Dane Palazzo <PalazzoDD@comcast.net>
2023-08-31 23:25:22 -0600

    phase-0-javascript-events-acting-on-events-lab complete

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
-// Your code here
+const dodger = document.getElementById("dodger");
+
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
+
+
+function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left < 360) {
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
+
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowRight") {
+      moveDodgerRight();
+    }
+  });
+
+
+/*
+Thought expriament that deosnt work in practice
+function moveDodger() {
+    const postitionNumbers = dodger.style.position.replace("px", "");
+    const position = parseInt(positionNumbers, 10);
+  
+    if (position > 0) {
+      dodger.style.position = `${position - 1}px`;
+    }
+  }
+    else if (position > 360) {
+      dodger.style.position = `${position + 1}px`;
+    }
+  }
+
+  */
